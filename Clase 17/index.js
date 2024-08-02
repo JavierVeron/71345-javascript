8c8de546c10d8451ead99721733afda0d73766fc
Javier Veron <javier.veron@gmail.com>
2024-08-02 01:03:11 -0300

    Clase 17

---
+++ Clase 17/index.js
@@ -0,0 +1,12 @@
+let academia = "Coderhouse";
+let mensaje = `Hola Chicos, estamos cursando JavaScript ${academia}`;
+console.log(mensaje);
+
+let bebida = {id:1, nombre:"Coca Cola", precio:1800};
+let bebidas = [
+    {id:1, nombre:"Coca Cola", precio:1800},
+    {id:2, nombre:"Pepsi", precio:1700},
+    {id:3, nombre:"Manaos", precio:1500},
+]
+console.log(bebida);
+console.log(bebidas);
